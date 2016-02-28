f8c1418290527606368b06dcae98e432296a775e
Joel Santos <joel@sendoushi.com>
2016-02-28 20:16:41 +0000

    [upd] Improve React build

--- config/mapping.js
+++ config/mapping.js
@@ -1,5 +1,11 @@
 const path = require('path');
 const deepMixIn = require('mout/object/deepMixIn.js');
 const bedrock = path.join(__dirname, '../node_modules/bedrock/config/mapping.js');
+const env = process.argv[2];
 
-module.exports = deepMixIn(require(bedrock), {});
+const prodMapping = env === 'prod' ? {
+    'react': 'node_modules/react/dist/react.min.js',
+    'react-dom': 'node_modules/react-dom/dist/react-dom.min.js'
+} : null;
+
+module.exports = deepMixIn(require(bedrock), prodMapping);
